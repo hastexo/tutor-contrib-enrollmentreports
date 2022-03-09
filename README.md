272840f06b02c2823de2099a568827895a563c71
Florian Haas <florian@citynetwork.eu>
2022-03-09 14:43:07 +0100

    build: Initial Git import
    Created with cookiecutter from gh:fghaas/cookiecutter-tutor-plugin.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Enrollment reports plugin for [Tutor](https://docs.tutor.overhang.io)
+
+## Installation
+
+    pip install git+https://github.com/hastexo/tutor-contrib-enrollmentreports
+
+## Usage
+
+    tutor plugins enable enrollmentreports
+
+## License
+
+This software is licensed under the terms of the AGPLv3.
---
+++ setup.py
@@ -0,0 +1,46 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def load_readme():
+    with io.open(os.path.join(HERE, "README.md"), "rt", encoding="utf8") as f:
+        return f.read()
+
+
+setup(
+    name="tutor-contrib-enrollmentreports",
+    use_scm_version=True,
+    url="https://github.com/hastexo/tutor-contrib-enrollmentreports",
+    project_urls={
+        "Code": "https://github.com/hastexo/tutor-contrib-enrollmentreports",
+        "Issue tracker": "https://github.com/hastexo/tutor-contrib-enrollmentreports/issues",  # noqa: E501
+    },
+    license="AGPLv3",
+    description="enrollmentreports plugin for Tutor",
+    long_description=load_readme(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    python_requires=">=3.6",
+    install_requires=["tutor"],
+    entry_points={
+        "tutor.plugin.v0": [
+            "enrollmentreports = tutorenrollmentreports.plugin"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
